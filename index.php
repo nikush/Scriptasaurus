53da52c920907c907506fa1cb4bca1d372bc4ff7
Nikush Patel <nikush.patel7@gmail.com>
2012-04-07 04:17:53 +0100

    Basic front controller implementation

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 require 'config/initialise.php';
+
+$page = (isset($_GET['page'])) ? $_GET['page'] : 'home';
+
+$controllerFile = 'application/controllers/' . $page . '.php';
+
+if (file_exists($controllerFile))
+{
+    include $controllerFile;
+} else
+{
+    include 'application/controllers/404.php';
+}
 ?>
