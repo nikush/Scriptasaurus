3e6168c08a7ed5c85aec6ad15242adb80cd1f16e
Nikush Patel <nikush.patel7@gmail.com>
2012-04-07 10:53:51 +0100

    Typo

--- application/pages/home/index.php
+++ application/pages/home/index.php
@@ -4,5 +4,5 @@
     <p>Through a series of fun, friendly and simple steps we will guide you through the wonderful world of JavaScript. And the end goal? Become a JavaScript master: a true Scriptasaurus.</p>
     <p>The course programme is designed specifically around you: set at your preferred pace and modified to reflect your most efficient learning style. So that with scriptasaurus.com you are guaranteed to learn in your most comfortable, productive and time-efficient manner.</p>
     <p>Please note that this course is aimed at coders with some knowledge of HTML and CSS, and to absolute beginners in JavaScript.</p>
-    <p>After all a Scriptasaurus isn’t born - <em>he is compiled.</em></p>
+    <p>After all a Scriptasaurus wasn’t born - <em>he was compiled.</em></p>
 </section>
