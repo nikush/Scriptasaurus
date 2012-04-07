0eff948dad50991db0ddb7dba0df30cb9b9ad425
Nikush Patel <nikush.patel7@gmail.com>
2012-04-07 10:29:25 +0100

    Moar CSS

--- application/pages/footer.php
+++ application/pages/footer.php
@@ -1,6 +1,13 @@
         </div> <!-- .content-container -->
         <footer>
-            <p>Links</p>
+            <ul>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+            </ul>
+            <p>Copyright &copy; 2012 Scripasaurus.com</p>
         </footer>
     </body>
 </html>
