56c890f2d1e92308013781cf9faa60fcf83f9725
Nikush Patel <nikush.patel7@gmail.com>
2012-04-07 04:29:36 +0100

    Basic header and footer inclusion

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -1,1 +1,3 @@
+<?php include 'application/pages/header.php'; ?>
 <h1>Home page</h1>
+<?php include 'application/pages/footer.php'; ?>
---
+++ application/pages/header.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="en_GB">
+    <head>
+        <meta charset="utf-8" />
+        <title>Scriptasaurus</title>
+    </head>
+    <body>
