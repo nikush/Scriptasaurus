34c2c1728eef57ca040265e82b9c81f7f8309fe5
Nikush Patel <nikush.patel7@gmail.com>
2012-04-12 21:38:25 +0100

    Finished sitemap structure

--- application/controllers/sitemap.php
+++ application/controllers/sitemap.php
@@ -1,5 +1,9 @@
 <?php getHeader(); ?>
-<div class="main">
-<?php include PAGES . 'sitemap/index.php'; ?>
+<section class="right-bar">
+    <?php include PAGES . 'enrolment-block.php'; ?>
+    <?php include PAGES . 'live-feed.php'; ?>
+</section>
+<div class="fluid main">
+    <?php include PAGES . 'sitemap/index.php'; ?>
 </div>
 <?php getFooter(); ?>
