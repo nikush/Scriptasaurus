b67bc5357f5c558d1a069a69b04bcebd6acecaa9
Nikush Patel <nikush.patel7@gmail.com>
2012-04-12 19:15:36 +0100

    Add enrolment to home page and slight restructure

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -1,8 +1,9 @@
 <?php getHeader(); ?>
+<section class="right-bar">
+    <?php include PAGES . 'enrolment-block.php'; ?>
+    <?php include PAGES . 'live-feed.php'; ?>
+</section>
 <div class="main">
-<?php include PAGES . 'live-feed.php'; ?>
-    <section class="right-bar">
     <?php include PAGES . 'home/index.php'; ?>
-    </section>
 </div>
 <?php getFooter(); ?>
