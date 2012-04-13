e138d3fb0ffe52330d987c17f3cdb5aa7a9897b5
Nikush Patel <nikush.patel7@gmail.com>
2012-04-13 19:44:32 +0100

    Start enrolment form

--- application/pages/enrol/index.php
+++ application/pages/enrol/index.php
@@ -1,4 +1,15 @@
 <div class="block">
     <h1>Enrol</h1>
     <p>Fill out and submit the form below to enrol on the Scriptasaurus Javascript course.</p>
+
+    <form method="post">
+        <label>Username</label>
+        <input type="text" />
+
+        <label>First Name</label>
+        <input type="text" />
+
+        <label>Surname</label>
+        <input type="text" />
+    </form>
 </div>
