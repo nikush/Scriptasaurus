f531e97e3a9e91ead368dc19ee07b72b6609f668
Nikush Patel <nikush.patel7@gmail.com>
2012-04-13 20:00:42 +0100

    Done form and styled form elements

--- application/pages/enrol/index.php
+++ application/pages/enrol/index.php
@@ -3,13 +3,32 @@
     <p>Fill out and submit the form below to enrol on the Scriptasaurus Javascript course.</p>
 
     <form method="post">
-        <label>Username</label>
-        <input type="text" />
-
         <label>First Name</label>
         <input type="text" />
 
         <label>Surname</label>
         <input type="text" />
+
+        <label>Email Address</label>
+        <input type="text" />
+
+        <label>Username</label>
+        <input type="text" />
+
+        <label>Password</label>
+        <input type="password" />
+
+        <label>Preferred Learning Pace</label>
+        <select>
+            <option>Daily Lessons</option>
+            <option>Weekly Lessons</option>
+            <option>Monthly Lessons</option>
+        </select>
+
+        <label>Mobile User</label>
+        <p><input type="checkbox" />Send lesson review quizzes to your mobile?</p>
+
+        <label>News Feed Notifications</label>
+        <p><input type="checkbox" />Receive email notifications?</p>
     </form>
 </div>
