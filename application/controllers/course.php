27041c19bc24ecf84f3fa3ab7ccd6dc4056da6a8
Nikush Patel <nikush.patel7@gmail.com>
2012-04-13 13:57:33 +0100

    Started course page

---
+++ application/controllers/course.php
@@ -0,0 +1,8 @@
+<?php getHeader(); ?>
+<section class="right-bar">
+    <?php include PAGES . 'live-feed.php'; ?>
+</section>
+<div class="main">
+    <?php include PAGES . 'course/index.php'; ?>
+</div>
+<?php getFooter(); ?>
---
+++ application/pages/course/index.php
@@ -0,0 +1,10 @@
+<div class="block fluid">
+    <h1>Course</h1>
+    <p>Welcome to the Scriptasaurus course page!</p>
+    <p>Our course covers beginner to advanced Javascript.</p>
+    <p>With lessons in...The Scriptasaurus javascript course will help you to understand, use, and
+implement this powerful scripting language in any web environment.</p>
+
+    <p>To enrol, just click the link below, and fill in our enrolment form, or, 
+if you are already a Scriptasaurus user, you can login to access your account below.</p>
+</div>
