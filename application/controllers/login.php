8fe3459c20dc16cf9450baef1b732abc83d5f582
Nikush Patel <nikush.patel7@gmail.com>
2012-04-15 14:30:55 +0100

    Begin basic login page

---
+++ application/controllers/login.php
@@ -0,0 +1,9 @@
+<?php
+$breadcrumbs = array(
+    'Home' => 'index.php',
+    'Login' => ''
+);
+?>
+<?php getHeader(); ?>
+<?php include PAGES . 'login/index.php'; ?>
+<?php getFooter(); ?>
---
+++ application/pages/login/index.php
@@ -0,0 +1,9 @@
+<div class="block">
+    <form>
+        <label>Usename</label>
+        <input type='text' />
+        <label>Password</label>
+        <input type='password' />
+        <input type='submit' />
+    </form>
+</div>
