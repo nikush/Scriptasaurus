d0fcc47cb1f2b42dfa622a1b944e01b545c0e1a2
Nikush Patel <nikush.patel7@gmail.com>
2012-04-16 16:24:57 +0100

    Add root path

--- config/paths.php
+++ config/paths.php
@@ -1,7 +1,11 @@
 <?php
 define('DS', DIRECTORY_SEPARATOR);
 
-define('APP', 'application' . DS);
+define('ROOT', dirname(dirname(__FILE__)) . DS);
+
+define('APP', ROOT . 'application' . DS);
 define('CONTROLLERS', APP . 'controllers' . DS);
 define('PAGES', APP . 'pages' . DS);
+
+define('SITE', ROOT . 'site' . DS);
 ?>
