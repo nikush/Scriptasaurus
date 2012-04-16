447a656251b7fc366b18c2f984bf9c49378f3de1
Nikush Patel <nikush.patel7@gmail.com>
2012-04-16 14:12:43 +0100

    Routing

--- index.php
+++ index.php
@@ -1,7 +1,24 @@
 <?php
 require 'config/initialise.php';
 
-$page = (isset($_GET['page'])) ? $_GET['page'] : 'home';
+//$page = (isset($_GET['page'])) ? $_GET['page'] : 'home';
+$urlStr = (isset($_GET['url'])) ? trim($_GET['url'], '/') : 'home';
+
+$urlChunks = explode('/', $urlStr);
+
+$url = array();
+$url['controller'] = $urlChunks[0];
+$url['action'] = null;
+$url['params'] = null;
+
+if (isset($urlChunks[1]))
+{
+    $url['action'] = $urlChunks[1];
+}
+
+print_r($url);
+
+die($urlStr);
 
 $controllerFile = CONTROLLERS . $page . '.php';
 
