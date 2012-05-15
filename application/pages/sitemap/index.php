85de041ed7cf7075d7888430629c78e2029e5522
Nikush Patel <nikush.patel7@gmail.com>
2012-05-15 09:46:23 +0100

    Begin dynamic sitemap

--- application/pages/sitemap/index.php
+++ application/pages/sitemap/index.php
@@ -1,3 +1,33 @@
+<?php
+$map = array(
+    'Home' => '',
+    'Course' => 'course',
+    array(
+        'Test Subpage' => 'subpage'
+    )
+);
+
+function listMap($map)
+{
+    foreach ($map as $pageName => $pageAddr)
+    {
+        echo '<li>';
+        if (is_array($pageAddr))
+        {
+            echo '<ul>';
+            listMap($pageAddr);
+            echo '</ul>';
+        }else
+        {
+            echo "<a href='".URLADDR."$pageAddr'>$pageName</a>";
+        }
+        echo '</li>';
+    }
+}
+?>
 <div class="block">
 <h1>Site Map</h1>
+<ul>
+<?php listMap($map); ?>
+</ul>
 </div>
