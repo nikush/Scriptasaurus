576b61662f0f507321b5076085efd1a1e03d0770
Nikush Patel <nikush.patel7@gmail.com>
2012-05-18 13:22:46 +0100

    Tutor status box

--- application/controllers/account.php
+++ application/controllers/account.php
@@ -6,6 +6,10 @@ $breadcrumbs = array(
 ?>
 <?php getHeader(); ?>
 <section class="right-bar">
+    <div class="block">
+        <h3>Tutor is online</h3>
+        <a href="#">Chat now!</a>
+    </div>
     <?php include PAGES . 'live-feed.php'; ?>
 </section>
 <div class="main">
