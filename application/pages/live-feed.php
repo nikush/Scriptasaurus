e6f404ab494ce597dc02f1b7f02cc8d47518e588
Nikush Patel <nikush.patel7@gmail.com>
2012-05-18 15:33:33 +0100

    Populate live feed

--- application/pages/live-feed.php
+++ application/pages/live-feed.php
@@ -1,3 +1,16 @@
 <div id="live-feed" class="block">
     <h3>Live feed</h3>
+    <ul>
+        <li><span class="purple">Kyle</span> failed the entire course</li>
+        <li><span class="purple">Jim</span> completed lesson <span class="purple">7</span></li>
+        <li><span class="purple">Raphael</span> completed lesson <span class="purple">5</span></li>
+        <li><span class="purple">Sunnefa</span> got her Scriptasaurus <span class="purple">certificate!</span></li>
+        <li><span class="purple">Aisha</span> completed lesson <span class="purple">3</span></li>
+        <li><span class="purple">Sooji</span> tweeted about <span class="purple">Scriptasaurus!</span></li>
+        <li><span class="purple">Chima</span> completed lesson <span class="purple">6</span></li>
+        <li><span class="purple">Deji</span> signed up!</li>
+        <li><span class="purple">Philip</span> loves <span class="purple">Scriptasaurus!</span></li>
+        <li><span class="purple">Itai</span> signed up!</li>
+        <li><span class="purple">Jennifer</span> completed lesson <span class="purple">1</span></li>
+    </ul>
 </div>
