fd4ea21b0333b0fce61e1bf4db728ada20a1ac16
Nikush Patel <nikush.patel7@gmail.com>
2012-05-18 12:56:55 +0100

    Rough start to account page

--- application/pages/account/index.php
+++ application/pages/account/index.php
@@ -1,3 +1,42 @@
-<section class="fluid block">
-    <h1>Account</h1>
+<section class="fluid">
+    <div class="block">
+        <h1>Welcome to your Scriptasaurus user account <?php echo getUser(); ?>!</h1>
+        <p>You last logged in on (date). 
+    The learning pace you have chosen is (learning pace). Change your learning pace here. 
+    You can also (Disable/Enable) your live feed here, and turn (on/off) notifications here. 
+    You are currently working on Lesson (1) of the Javascript course.</p>
+    </div>
+    <div class="block">
+        <h2>Lessons</h2>
+        <table>
+            <thead>
+                <tr>
+                    <td>Link</td>
+                    <td>Description</td>
+                    <td>Feedback</td>
+                    <td>Discussion</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td><a href="#">Lesson 1</a></td>
+                    <td>Description</td>
+                    <td>Feedback</td>
+                    <td>Discussion</td>
+                </tr>
+                <tr>
+                    <td>Lesson 2</td>
+                    <td>Description</td>
+                    <td>Feedback</td>
+                    <td>Discussion</td>
+                </tr>
+                <tr>
+                    <td>Lesson 3</td>
+                    <td>Description</td>
+                    <td>Feedback</td>
+                    <td>Discussion</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </section>
