996aa8070d437eb33d13877e83e959f89819653c
Nikush Patel <nikush.patel7@gmail.com>
2012-05-20 17:31:25 +0100

    Tidy up lists everywhere

--- application/pages/live-feed.php
+++ application/pages/live-feed.php
@@ -1,6 +1,6 @@
 <div id="live-feed" class="block">
     <h3>Live feed</h3>
-    <ul>
+    <ul class="unstyled">
         <li><span class="purple">Kyle</span> failed the entire course</li>
         <li><span class="purple">Jim</span> completed lesson <span class="purple">7</span></li>
         <li><span class="purple">Raphael</span> completed lesson <span class="purple">5</span></li>
